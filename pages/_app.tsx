f22af10482149e622968752f9f15e509509b10b5
Or Druker <or@druker.net>
2022-12-04 15:22:39 +0200

    add google fonts
    Signed-off-by: Or Druker <or@druker.net>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Barlow, Fraunces } from "@next/font/google";
+
+const barlow = Barlow({
+  weight: ["600"],
+  variable: "--font-barlow",
+});
+
+const fraunces = Fraunces({
+  weight: ["700", "900"],
+  variable: "--font-fraunces",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={`${barlow.variable} ${fraunces.variable}`}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-barlow)", ...fontFamily.sans],
+        serif: ["var(--font-fraunces)", ...fontFamily.serif],
+      },
+    },
   },
   plugins: [],
-}
+};
