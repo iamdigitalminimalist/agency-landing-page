3d21392804a1c1d7454c3ab4baf6703babcd28c9
Or Druker <or@druker.net>
2022-12-04 15:34:58 +0200

    add custom colors
    Signed-off-by: Or Druker <or@druker.net>

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,17 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      "custom-soft-red": "#fe7867",
+      "custom-yellow": "#fad400",
+      "custom-dark-desaturated-cyan": "#25564b",
+      "custom-dark-moderate-cyan": "#458c7e",
+      "custom-dark-blue": "#19536b",
+      "custom-extra-dark-desaturated-blue": "#5a636c",
+      "custom-extra-dark-grayish-blue": "#818498",
+      "custom-grayish-blue": "#a7abae",
+      "custom-white": "#ffffff",
+    },
     extend: {
       fontFamily: {
         sans: ["var(--font-barlow)", ...fontFamily.sans],
